d65c2513a390656ecded7465d9e08225ed3bf3f9
ljy1020 <15050593078@139.com>
2020-01-05 14:40:20 +0800

    Initial commit

---
+++ src/main/java/com/istimeless/nettylearn/firstexample/TestServer.java
@@ -0,0 +1,26 @@
+package com.istimeless.nettylearn.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestChildHandler());
+            ChannelFuture f = b.bind(8899).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/istimeless/nettylearn/secondexample/MyClient.java
@@ -0,0 +1,8 @@
+package com.istimeless.nettylearn.secondexample;
+
+public class MyClient {
+
+    public static void main(String[] args) {
+
+    }
+}
