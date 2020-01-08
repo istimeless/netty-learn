990271a72d45307731dc245854fcd4b057c45a2d
istimeless <istimeless@foxmail.com>
2020-01-08 19:49:42 +0800

    sixth example

--- src/main/java/com/istimeless/nettylearn/sixthexample/TestClientHandler.java
+++ src/main/java/com/istimeless/nettylearn/sixthexample/TestClientHandler.java
@@ -8,4 +8,10 @@ public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.Stud
     protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
 
     }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        DataInfo.Student student = DataInfo.Student.newBuilder().setName("张三").setAge(20).setAddress("北京").build();
+        ctx.writeAndFlush(student);
+    }
 }
--- src/main/java/com/istimeless/nettylearn/sixthexample/TestServerHandler.java
+++ src/main/java/com/istimeless/nettylearn/sixthexample/TestServerHandler.java
@@ -9,6 +9,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
-        
+        System.out.println(msg.getName());
+        System.out.println(msg.getAge());
+        System.out.println(msg.getAddress());
     }
 }
